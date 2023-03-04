ce3d7e7efc4a464978e208bf7d787b0adcf9ebdc
Tony Lau <ual_ynot@yahoo.com>
2023-03-04 15:00:00 -0500

    Restructured project

---
+++ src/ast.rs
@@ -0,0 +1,108 @@
+
+#[derive(Debug, PartialEq)]
+pub enum Definition {
+    Type(TypeDef),
+    Const(ConstDef),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct TypeDef {
+    pub name:   String,
+    pub params: Vec<String>,
+    pub expr:   TypeExpr,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct ConstDef {
+    pub name:        String,
+    pub type_expr:   Option<TypeExpr>,
+    pub type_params: Vec<String>,
+    pub expr:        ValueExpr,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum TypeExpr {
+    Variable(String, Vec<TypeExpr>),
+    Universal(String),
+    Existential(String),
+    Tuple(Vec<TypeExpr>),
+    Struct(Vec<(String, TypeExpr)>),
+    Choice(Vec<TypeExpr>),
+    Tagged(Vec<(String, TypeExpr)>),
+    Function(Box<TypeExpr>, Box<TypeExpr>),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct ValueExpr {
+    pub variant:   ExprVariant,
+    pub type_expr: Option<TypeExpr>,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum ExprVariant { // TODO flesh these out
+    Variable(String),
+    Literal(LitVariant),
+    BinaryOp(BinOpVariant, Box<ValueExpr>, Box<ValueExpr>),
+    UnaryOp(UnOpVariant, Box<ValueExpr>),
+    Tuple(Vec<ValueExpr>),
+    Struct(Vec<(String, ValueExpr)>),
+    Choice(Box<ValueExpr>),
+    Tagged(String, Box<ValueExpr>),
+    Closure,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum LitVariant {
+    Bool(bool),
+    Integer(i32),
+    Float(f32),
+    Ascii(Vec<u8>),
+    U8Char(u8),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum BinOpVariant {
+    Pow,
+    Log,
+    Mul,
+    Div,
+    Mod,
+    Add,
+    Sub,
+    Gt,
+    Lt,
+    Equ,
+    Neq,
+    And,
+    Or,
+    Call,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum UnOpVariant {
+    Cast,
+    Ref,
+    Deref,
+    Pos,
+    Neg,
+    Not,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Closure {
+    pub name:    String,
+    pub params:  Vec<(String, TypeExpr)>,
+    pub returns: TypeExpr,
+    pub body:    Vec<Statement>,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Statement { // TODO flesh these out
+    Let,
+    Assignment,
+    Expression, // TODO in AST check call must have at least one mutable argument
+    Return,
+    Condition,
+    Loop,
+}
+
